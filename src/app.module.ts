3219ae571934d423e4ac5dfd294c867207b0a006
minskim2 <nicek789@naver.com>
2023-01-24 21:07:29 +0900

    feat: postgresql 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,11 +11,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 		UserModule,
 		TypeOrmModule.forRoot({
 			type: 'postgres',
-			host: 'localhost',
-			port: 5432,
-			username: 'postgres',
-			password: '1436',
-			database: 'tscen',
+			host: process.env.DB_HOST,
+			port: Number(process.env.DB_PORT),
+			username: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_NAME,
 			synchronize: true,
 			// 데이터 베이스에서 모듈의 현재상태로 마이크레이션
 			logging: false,
