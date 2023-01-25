8c8c1979d019515781baeb97068280a2ee4d5c5a
feedbaek <nicek789@naver.com>
2023-01-26 00:48:29 +0900

    fix: env 환경변수명 오류 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,11 +11,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 		UserModule,
 		TypeOrmModule.forRoot({
 			type: 'postgres',
-			host: process.env.POSTGRES_HOST,
-			port: Number(process.env.POSTGRES_PORT),
-			username: process.env.POSTGRES_USER,
+			host: process.env.PSQL_HOST,
+			port: Number(process.env.PSQL_PORT),
+			username: process.env.PSQL_USER,
 			password: process.env.POSTGRES_PASSWORD,
-			database: process.env.POSTGRES_NAME,
+			database: process.env.PSQL_DB,
 			synchronize: true,
 			// 데이터 베이스에서 모듈의 현재상태로 마이크레이션
 			logging: false,
